ef0480e2bc65df66862ed271cf187f68f591d9ca
SandhyaJ0304 <sandhyajadhao0303@gmail.com>
2025-02-21 17:21:12 +0530

    Added headless mode in chrome

--- WeatherApplication/src/test/java/utility/BrowserDriver.java
+++ WeatherApplication/src/test/java/utility/BrowserDriver.java
@@ -2,13 +2,16 @@ package utility;  // You can change the package name based on your structure
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-public class BrowserDriver {
+public class BrowserDriver
+{
 
     // Static method to get the WebDriver instance
-    public static WebDriver GetDriver() {
-        // Set the path to your WebDriver executable (e.g., chromedriver.exe)
-        // Update with the actual path
-        return new ChromeDriver(); // Return an instance of ChromeDriver (WebDriver)
+    public static WebDriver GetDriver()
+    {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        return new ChromeDriver(options); // Return an instance of ChromeDriver (WebDriver)
     }
 }
