0b5b1dbf731c7f2adef9cf3e9161d5cbf832243a
SandhyaJ0304 <sandhyajadhao0303@gmail.com>
2025-02-23 16:47:10 +0530

    Added RemoteWebDriver

--- WeatherApplication/src/test/java/utility/BrowserDriver.java
+++ WeatherApplication/src/test/java/utility/BrowserDriver.java
@@ -3,6 +3,7 @@ package utility;  // You can change the package name based on your structure
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BrowserDriver
 {
@@ -12,7 +13,7 @@ public class BrowserDriver
     {
         ChromeOptions options = new ChromeOptions();
         //options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
-        
-        return new ChromeDriver(options); // Return an instance of ChromeDriver (WebDriver)
+
+        return new RemoteWebDriver(options); // Return an instance of ChromeDriver (WebDriver)
     }
 }
